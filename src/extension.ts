96d5a59465850a574df040ee4d49d3d286ca0ca9
phantola <devmonarch0115@gmail.com>
2022-12-03 14:16:09 +0900

    Update

--- src/extension.ts
+++ src/extension.ts
@@ -1,30 +1,46 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import dayjs = require("dayjs");
 import * as vscode from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "timespace" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand(
-        "timespace.helloWorld",
-        () => {
-            // The code you place here will be executed every time your command is executed
-            // Display a message box to the user
-            vscode.window.showInformationMessage(
-                `TimeSpace is Activated! ${new Date().toString()}`
-            );
-        }
+let utcTimeItem: vscode.StatusBarItem;
+let localeTimeItem: vscode.StatusBarItem;
+export function activate(context: vscode.ExtensionContext) {
+    const subscriptions = context.subscriptions;
+    vscode.window.showInformationMessage(`TimeSpace is now activated!`);
+
+    utcTimeItem = vscode.window.createStatusBarItem(
+        vscode.StatusBarAlignment.Left,
+        20
+    );
+    localeTimeItem = vscode.window.createStatusBarItem(
+        vscode.StatusBarAlignment.Left,
+        20
     );
 
-    context.subscriptions.push(disposable);
+    subscriptions.push(utcTimeItem);
+    subscriptions.push(localeTimeItem);
+
+    // update status bar item per second
+    setInterval(() => updateStatusBarItem(), 1000);
+}
+
+function updateStatusBarItem(): void {
+    const userTimeZoneOffsetHour = new Date().getTimezoneOffset() / 60;
+
+    utcTimeItem.text = `$(clock) U ${dayjs()
+        .subtract(-userTimeZoneOffsetHour, "hours")
+        .format("YYYY-MM-DD HH:mm:ss")}`;
+    localeTimeItem.text = `$(clock) L ${dayjs().format("YYYY-MM-DD HH:mm:ss")}`;
+
+    utcTimeItem.show();
+    localeTimeItem.show();
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+    vscode.window.showInformationMessage(`TimeSpace is now deactivated.`);
+}
